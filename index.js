d9c568b118fe6d9a03cf9363bdbac1f06d50a1bf
ThymenVg <tvangijselk@gmail.com>
2021-07-18 19:59:58 +0200

    f

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static("./public/Login_v10"));
 
 const port = process.env.PORT || 8130; 
 app.listen(port, console.log("connected.."));
