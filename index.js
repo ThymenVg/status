3d1571dac43ec13ddeb86f2a7c38d256861c9940
technosander <sander4lauwers@gmail.com>
2021-07-24 23:20:49 +0200

    Added chat functionality

--- index.js
+++ index.js
@@ -17,21 +17,32 @@ app.get("/happiness", (req, res) => {
         happiness: happiness
     });
 })
+app.get("/socket.io/:filename", (req, res) => {
+    const filename = req.params.filename;
+    res.sendFile(path.join(__dirname, "node_modules", "socket.io", "client-dist", filename))
+});
 
 app.post("/updateHappiness", (req, res) => {
     let body = res.body;
     happiness = body.happinessValue;
 });
 
-io.on("connection", socket => {
-    // send ALL (maybe limited amount?) msgs to socket.id from database
 
-    socket.on("message", msg => {
+let messageObjects = [];
+io.on("connection", socket => {
+    console.log(`user ${socket.id} connected`);
+    // send ALL (maybe limited amount?) msgs to socket.id from database
+    socket.emit("allMessages", messageObjects);
+
+    socket.on("message", msgObj => {
+        console.log(msgObj);
         // send this msg to all connected users, they append (sender does so locally)
+        socket.broadcast.emit("message", msgObj);
         // store msg in database
+        messageObjects.push(msgObj);
     });
 });
 
 
 const port = process.env.PORT || 8130;
-app.listen(port, console.log(`connected...\nLink: http://localhost:${port}`));
+server.listen(port, console.log(`connected...\nLink: http://localhost:${port}`));
